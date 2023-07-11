b0311f716c45c24ef49ebaa1aeb711db9dc342ff
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-11 16:18:41 -0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+
 const form = document.getElementById('novoItem');
 const list = document.getElementsById('lista')
+const itens = JSON.parse(localStorage.getItem('')) || [];
+
+itens.forEach( (element) => {
+    
+} );
 
 form.addEventListener('submit', (ev) => {
     i.preventDefault();
     
-    createElement(ev.target.elements['nome'].value, ev.target.elements['quantidade'].value);
+    const nome = ev.target.elements['nome'];
+    const quantidade = ev.target.elements['quantidade'];
+
+    createElement(nome.value, quantidade.value);
+
+     nome.value = ''
+     quantidade.value = ''
 });
 
 function createElement(nome, quantidade) {
@@ -18,6 +30,14 @@ function createElement(nome, quantidade) {
     newItem.appendChild(numberItem)
     newItem.innerHTML += nome;
 
-    list.appendChild(newItem)
+    list.appendChild(newItem);
 
+    const currentItem = {
+        'nome': nome,
+        'quantidade': quantidade
+    }
+
+    itens.push(currentItem)
+    
+    localStorage.setItem('itens',JSON.stringify(itens));
 }
