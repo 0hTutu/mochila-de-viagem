28bbafbdb05917d0494b3c898b658d1b55acbb26
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-11 15:16:26 -0300

    update index.html & main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const form = document.getElementById('novoItem');
 
-form.addEventListener('submit', (i) => {
+form.addEventListener('submit', (ev) => {
     i.preventDefault();
     
-    console.log(i.target.elements['nome'].value);
-    console.log(i.target.elements['quantidade'].value);
+    createElement(ev.target.elements['nome'].value, ev.target.elements['quantidade'].value);
 });
+
+function createElement(nome, quantidade) {
+    console.log(nome);
+    console.log(quantidade);
+
+//<li class="item"><strong>7</strong>Camisas</li>
+    const newItem = document.createElement('li');
+    newItem.classList.add('item');
+
+    const numberItem = document.createElement('strong');
+    numberItem.innerHTML = quantidade;
+
+    newItem.appendChild(numberItem)
+    newItem.innerHTML += nome;
+
+}
