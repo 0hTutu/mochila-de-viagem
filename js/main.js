b2d036da0d26f25808ca48da1e2be452281f09de
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-11 15:03:15 -0300

    update mainjs

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ const form = document.getElementById('novoItem');
 
 form.addEventListener('submit', (i) => {
     i.preventDefault();
-    console.log('zap')
+    
+    console.log(i.target.elements['nome'].value);
+    console.log(i.target.elements['quantidade'].value);
 });
