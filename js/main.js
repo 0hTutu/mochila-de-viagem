ab158c1ebd1c38a00af032ddd3977f4f999b864c
Arthur Müller <112774071+0hTutu@users.noreply.github.com>
2023-07-11 16:46:35 -0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -13,14 +13,23 @@ form.addEventListener('submit', (ev) => {
     const nome = ev.target.elements['nome'];
     const quantidade = ev.target.elements['quantidade'];
 
+    const existe = itens.find( element => element.nome === nome.value);
+
     const currentItem = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
 
+    if(existe){
+        currentItem.id = existe.id
+    } else {
+        currentItem.id = itens.length
+        createElement(currentItem);
+        itens.push(currentItem)
+    }
+
     createElement(currentItem);
 
-    
     itens.push(currentItem)
     
     localStorage.setItem('itens',JSON.stringify(itens));
@@ -35,9 +44,11 @@ function createElement(item) {
 
     const numberItem = document.createElement('strong');
     numberItem.innerHTML = item.quantidade;
-
+    numberItem.dataset.id = item.id
     newItem.appendChild(numberItem)
+    
     newItem.innerHTML += item.nome;
 
     list.appendChild(newItem);
 }
+
